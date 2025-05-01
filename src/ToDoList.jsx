d845b92f1a1f86f510766726d0dcc7c401286f62
Mayank Verma <mayankverma.dev@gmail.com>
2025-05-01 15:49:09 +0530

    added URL

--- src/ToDoList.jsx
+++ src/ToDoList.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from 'react'
 import { toast } from 'react-toastify';
 import io from 'socket.io-client';
-const socket = io('http://localhost:4001');
+const socket = io('https://crudsocket.netlify.app/');
 export default function ToDoList() {
     const [toDoData, setToDoData] = useState([
         { name: 'Mayank', id: 1 },
